bf2179802a0d8bfa2c8c6e80080c9cdd02069046
mixedrgb <88474004+mixedrgb@users.noreply.github.com>
2023-06-17 22:09:43 -0500

    Rename ThindDo.cs to CoffeeQuery.cs

--- CoffeeQuery.cs
+++ CoffeeQuery.cs
@@ -12,30 +12,37 @@ public static class CoffeeQuery
         {
             sql.Append(" CoffeeName = @CoffeeName");
         }
+
         if (cofe.Region != null && cofe.Region.Length > 0)
         {
             sql.Append(", Region = @Region");
         }
+
         if (cofe.Processing != null && cofe.Processing.Length > 0)
         {
             sql.Append(", Processing = @Processing,");
         }
+
         if (cofe.Varietal != null && cofe.Varietal.Length > 0)
         {
             sql.Append(", Varietal = @Varietal");
         }
+
         if (cofe.RoastType != null && cofe.RoastType.Length > 0)
         {
             sql.Append(", RoastType = @RoastType");
         }
+
         if (cofe.Weight != null && cofe.Weight.Length > 0)
         {
             sql.Append(", Weight = @Weight");
         }
+
         if (cofe.RoastDate != null && cofe.RoastDate.Length > 0)
         {
             sql.Append(", RoastDate = @RoastDate");
         }
+
         sql.Append(" WHERE CoffeeId = @CoffeeId");
 
         return sql.ToString();
